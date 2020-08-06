8eeb47c0862a1e3d457af33502fca450a1357a7a
zhaobingkun <zhaobingkun@139.com>
2020-08-06 09:54:26 +0800

    flink 練習

--- FlinkStudy/flink-stream/src/main/java/com/flinkstudy/stream/DataStreamKeyByOperator.java
+++ FlinkStudy/flink-stream/src/main/java/com/flinkstudy/stream/DataStreamKeyByOperator.java
@@ -12,6 +12,22 @@ import javax.xml.crypto.dsig.keyinfo.KeyInfo;
 import java.util.Arrays;
 
 public class DataStreamKeyByOperator {
+
+    /**
+     *
+     * KeyBy: 按指定的Key对数据重分区。将同一Key的数据放到同一个分区。
+     *
+     * 注意:
+     *
+     * 分区结果和KeyBy下游算子的并行度强相关。如下游算子只有一个并行度,不管怎么分，都会分到一起。
+     * 对于POJO类型，KeyBy可以通过keyBy(fieldName)指定字段进行分区。
+     * 对于Tuple类型，KeyBy可以通过keyBy(fieldPosition)指定字段进行分区。
+     * 对于一般类型，如上, KeyBy可以通过keyBy(new KeySelector {…})指定字段进行分区。
+     *
+     * Summary:
+     *      KeyBy: 按指定的Key对数据重分区。将同一Key的数据放到同一个分区。
+     */
+
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         // 输入: 用户行为。某个用户在某个时刻点击或浏览了某个商品，以及商品的价格。
