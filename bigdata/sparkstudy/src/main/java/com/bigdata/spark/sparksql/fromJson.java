352060d42f074af72f2a6cc4e43dcbf17842611d
zhaobingkun <zhaobingkun@139.com>
2021-01-28 11:03:03 +0800

    spark sql 练习

--- bigdata/sparkstudy/src/main/java/com/bigdata/spark/sparksql/fromJson.java
+++ bigdata/sparkstudy/src/main/java/com/bigdata/spark/sparksql/fromJson.java
@@ -4,15 +4,19 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.SparkSession;
 
 public class fromJson {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setMaster("local").setAppName("sql");
         JavaSparkContext sc = new JavaSparkContext(conf);
         SQLContext sqlContext = new SQLContext(sc);
-        Dataset df = sqlContext.read().csv("E:\\奈学培训\\大数据开发\\第23次\\emp.json");
+        Dataset df = sqlContext.read().json("E:\\奈学培训\\大数据开发\\第23次\\emp.json");
         df.show();//show默认显示20行，可以跟数字
-        df.printSchema();
+        //df.printSchema();
+        SparkSession spark = SparkSession.builder().config(conf).getOrCreate();
+        df.createOrReplaceTempView("emp");
+        spark.sql("select * from emp where sal >2000").show();
         }
 
 }
