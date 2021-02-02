2f81e806108bfc2c291adc2e37b3b85adaf99a75
zhaobingkun <zhaobingkun@139.com>
2021-02-02 16:13:24 +0800

    spark作业 使用SparkSQL求每门课程中成绩最好的前三名

--- bigdata/sparkstudy/src/main/java/com/bigdata/spark/sparksql/sparkScore.java
+++ bigdata/sparkstudy/src/main/java/com/bigdata/spark/sparksql/sparkScore.java
@@ -6,19 +6,21 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.SparkSession;
 
-public class fromJson {
+public class sparkScore {
+
     public static void main(String[] args) {
+
         SparkConf conf = new SparkConf().setMaster("local").setAppName("sql");
         JavaSparkContext sc = new JavaSparkContext(conf);
         SQLContext sqlContext = new SQLContext(sc);
-        Dataset df = sqlContext.read().json("E:\\奈学培训\\大数据开发\\第23次\\emp.json");
+        Dataset df = sqlContext.read().json("E:\\奈学培训\\大数据开发\\第24次\\score.json");
         df.show();//show默认显示20行，可以跟数字
-        //df.printSchema();
+        df.printSchema();
         SparkSession spark = SparkSession.builder().config(conf).getOrCreate();
-        df.createOrReplaceTempView("emp");
+        df.createOrReplaceTempView("score");
+        spark.sql("select  t1.course course,  t1.name name,  t1.score score  from ( select  course,  name,  score,  row_number() over(partition by course order by score desc ) top  from score) t1 where t1.top<=3").show();
 
-        spark.sql("select * from emp where deptno = 10").show();
+
 
     }
-
 }
