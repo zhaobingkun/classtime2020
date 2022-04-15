0556d17103c0cec6b82abd62e994055389d5a5f7
zhaobingkun <zhaobingkun@139.com>
2022-04-15 09:40:09 +0800

    sqlserver 2 clickhouse

--- sqlServer2ClickHouse/src/main/java/com/cmei/s2c/JsonDebeziumDeserializationSchema.java
+++ sqlServer2ClickHouse/src/main/java/com/cmei/s2c/JsonDebeziumDeserializationSchema.java
@@ -6,16 +6,10 @@ import io.debezium.data.Envelope;
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.util.Collector;
-
 import org.apache.kafka.connect.data.Field;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.Struct;
-
 import org.apache.kafka.connect.source.SourceRecord;
-
-
-
-
 import java.util.HashMap;
 
 public class JsonDebeziumDeserializationSchema implements DebeziumDeserializationSchema {
